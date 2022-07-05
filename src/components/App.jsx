e29e630476346613b8f5453c9eabfb95f0b3172c
DenysTutov <93935076+DenysTutov@users.noreply.github.com>
2022-07-05 11:06:48 +0300

    Bugfix import React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import { Component } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedBackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 
-class App extends React.Component {
+class App extends Component {
   state = { good: 0, neutral: 0, bad: 0 };
 
   handleLeaveFeedback = event => {
